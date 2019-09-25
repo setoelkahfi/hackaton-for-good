b47f296c2242f8a2a570defadc2f3a8be176f693
Seto Elkahfi <setoelkahfi@gmail.com>
2019-09-25 09:50:37 +0200

    Initial Commit

---
+++ HackatonForGood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackatonForGood
+//
+//  Created by Seto Elkahfi on 25/09/19.
+//  Copyright © 2019 Seto Elkahfi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
