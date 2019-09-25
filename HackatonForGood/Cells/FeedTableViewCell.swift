29951b4cfeeaa78310f878dd77e53495d0b5e3d7
Seto Elkahfi <setoelkahfi@gmail.com>
2019-09-25 14:44:59 +0200

    add table view

---
+++ HackatonForGood/Cells/FeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedTableViewCell.swift
+//  HackatonForGood
+//
+//  Created by Seto Elkahfi on 25/09/19.
+//  Copyright © 2019 Seto Elkahfi. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- HackatonForGood/ViewControllers/EventViewController.swift
+++ HackatonForGood/ViewControllers/EventViewController.swift
@@ -9,7 +9,38 @@
 import Foundation
 import UIKit
 
-class EventViewController: UIViewController {
+class EventViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        let feedTableViewCell = UINib(nibName: "FeedTableViewCell", bundle: nil)
+        tableView.register(feedTableViewCell, forCellReuseIdentifier: "FeedTableViewCell")
+        
+        
+        
+    }
+    
+    
+    // TableView
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedTableViewCell", for: indexPath) as! FeedTableViewCell
+        
+        
+        
+        return cell
+    }
+    
+    
+    
 }
